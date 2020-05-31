b97991e8630dc35ee4e9663e9f09dc7630320edb
Lucas de Lucca <llucca54@gmail.com>
2020-05-31 10:23:31 -0300

    dashboard and import pages

--- src/components/Upload/index.tsx
+++ src/components/Upload/index.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from 'react';
-
 import Dropzone from 'react-dropzone';
+
 import { DropContainer, UploadMessage } from './styles';
 
 interface UploadProps {
@@ -27,7 +27,10 @@ const Upload: React.FC<UploadProps> = ({ onUpload }: UploadProps) => {
 
   return (
     <>
-      <Dropzone accept=".csv, application/vnd.ms-excel, text/csv" onDropAccepted={(files) => onUpload(files)}>
+      <Dropzone
+        accept=".csv, application/vnd.ms-excel, text/csv"
+        onDropAccepted={files => onUpload(files)}
+      >
         {({ getRootProps, getInputProps, isDragActive, isDragReject }): any => (
           <DropContainer
             {...getRootProps()}
--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -3,14 +3,12 @@ import { useHistory } from 'react-router-dom';
 
 import filesize from 'filesize';
 
-import Header from '../../components/Header';
-import FileList from '../../components/FileList';
-import Upload from '../../components/Upload';
-
-import { Container, Title, ImportFileContainer, Footer } from './styles';
-
 import alert from '../../assets/alert.svg';
+import FileList from '../../components/FileList';
+import Header from '../../components/Header';
+import Upload from '../../components/Upload';
 import api from '../../services/api';
+import { Container, Title, ImportFileContainer, Footer } from './styles';
 
 interface FileProps {
   file: File;
@@ -23,19 +21,34 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    if (!uploadedFiles.length) return;
+
+    const [file] = uploadedFiles;
+
+    data.append('file', file.file, file.name);
 
     try {
-      // await api.post('/transactions/import', data);
-    } catch (err) {
-      // console.log(err.response.error);
+      await api.post('transactions/import', data);
+
+      history.push('/');
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.log(error);
     }
+
+    // TODO
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const formattedUploadedFiles: FileProps[] = files.map(file => ({
+      file,
+      name: file.name,
+      readableSize: filesize(file.size),
+    }));
+
+    setUploadedFiles(formattedUploadedFiles);
   }
 
   return (
